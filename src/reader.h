7da94589c262f3a661cea34fe1300ceeb72eb92c
Qi Gao <qi.gao2012@gmail.com>
2026-09-23 16:11:14 +0800

    feat(reader): add signed LEB32 and LEB64 decoding

--- src/reader.h
+++ src/reader.h
@@ -17,6 +17,8 @@ size_t turbowasm_reader_remaining(const turbowasm_reader *reader);
 bool turbowasm_reader_u8(turbowasm_reader *reader, uint8_t *out);
 bool turbowasm_reader_u32le(turbowasm_reader *reader, uint32_t *out);
 bool turbowasm_reader_uleb32(turbowasm_reader *reader, uint32_t *out);
+bool turbowasm_reader_sleb32(turbowasm_reader *reader, int32_t *out);
+bool turbowasm_reader_sleb64(turbowasm_reader *reader, int64_t *out);
 bool turbowasm_reader_slice(turbowasm_reader *reader,
                             size_t size,
                             turbowasm_reader *out);
