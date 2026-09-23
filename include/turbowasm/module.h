84e9f75416fdcca6092d3c6478f8906eb94eaca4
Qi Gao <qi.gao2012@gmail.com>
2026-09-23 16:16:53 +0800

    feat(module): expose segment counts in validation summary

--- include/turbowasm/module.h
+++ include/turbowasm/module.h
@@ -35,6 +35,8 @@ typedef struct turbowasm_module_summary {
     uint32_t code_count;
 
     uint32_t export_count;
+    uint32_t element_count;
+    uint32_t data_segment_count;
     bool has_start;
     uint32_t start_function_index;
     bool has_data_count;
