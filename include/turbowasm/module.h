aa3b78262b4e92df02a0511dc2ee3df34808730d
Qi Gao <qi.gao2012@gmail.com>
2026-09-23 15:44:16 +0800

    feat(validation): extend module linkage summary

--- include/turbowasm/module.h
+++ include/turbowasm/module.h
@@ -20,9 +20,25 @@ typedef struct turbowasm_module {
 typedef struct turbowasm_module_summary {
     uint32_t standard_section_mask;
     size_t custom_section_count;
+
     uint32_t type_count;
+
+    uint32_t imported_function_count;
+    uint32_t imported_table_count;
+    uint32_t imported_memory_count;
+    uint32_t imported_global_count;
+
     uint32_t function_count;
+    uint32_t table_count;
+    uint32_t memory_count;
+    uint32_t global_count;
     uint32_t code_count;
+
+    uint32_t export_count;
+    bool has_start;
+    uint32_t start_function_index;
+    bool has_data_count;
+    uint32_t data_count;
 } turbowasm_module_summary;
 
 turbowasm_status turbowasm_module_load_borrowed(turbowasm_module *module,
