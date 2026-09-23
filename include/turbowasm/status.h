2cdb8b573a65aff47a8dad279c5a7e47b1e74b92
Qi Gao <qi.gao2012@gmail.com>
2026-09-23 15:01:56 +0800

    feat(runtime): add status contract

---
+++ include/turbowasm/status.h
@@ -0,0 +1,23 @@
+#ifndef TURBOWASM_STATUS_H
+#define TURBOWASM_STATUS_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef enum turbowasm_status {
+    TURBOWASM_OK = 0,
+    TURBOWASM_INVALID_ARGUMENT,
+    TURBOWASM_MALFORMED_MODULE,
+    TURBOWASM_UNSUPPORTED,
+    TURBOWASM_OUT_OF_MEMORY,
+    TURBOWASM_TYPE_MISMATCH
+} turbowasm_status;
+
+const char *turbowasm_status_string(turbowasm_status status);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* TURBOWASM_STATUS_H */
