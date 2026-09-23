86d78049b91763e8cf3bc625ddd7653d06b8026d
Qi Gao <qi.gao2012@gmail.com>
2026-09-23 22:43:25 +0800

    feat(runtime): retain table and memory limits

--- src/validation_context.h
+++ src/validation_context.h
@@ -47,13 +47,21 @@ typedef struct turbowasm_validation_global {
     bool imported;
 } turbowasm_validation_global;
 
+typedef struct turbowasm_validation_limits {
+    uint32_t minimum;
+    uint32_t maximum;
+    bool has_maximum;
+} turbowasm_validation_limits;
+
 typedef struct turbowasm_validation_table {
     uint8_t reference_type;
     bool imported;
+    turbowasm_validation_limits limits;
 } turbowasm_validation_table;
 
 typedef struct turbowasm_validation_memory {
     bool imported;
+    turbowasm_validation_limits limits;
 } turbowasm_validation_memory;
 
 typedef struct turbowasm_validation_context {
@@ -120,10 +128,12 @@ bool turbowasm_validation_context_append_global(
 bool turbowasm_validation_context_append_table(
     turbowasm_validation_context *context,
     uint8_t reference_type,
+    turbowasm_validation_limits limits,
     bool imported);
 
 bool turbowasm_validation_context_append_memory(
     turbowasm_validation_context *context,
+    turbowasm_validation_limits limits,
     bool imported);
 
 turbowasm_validation_function *
