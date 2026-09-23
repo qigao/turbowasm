5b3281f9bbc0473a21e777e2773da9d0e8dcf572
Qi Gao <qi.gao2012@gmail.com>
2026-09-23 18:06:31 +0800

    feat(validation): expose reusable module const-expression validator

--- src/validate_data.h
+++ src/validate_data.h
@@ -7,6 +7,11 @@
 #include "reader.h"
 #include "validation_context.h"
 
+turbowasm_status turbowasm_validate_const_expr(
+    turbowasm_reader *reader,
+    turbowasm_validation_context *context,
+    uint8_t *out_type);
+
 turbowasm_status turbowasm_validate_global_section(
     turbowasm_reader *section,
     turbowasm_module_summary *summary,
@@ -14,6 +19,6 @@ turbowasm_status turbowasm_validate_global_section(
 turbowasm_status turbowasm_validate_data_section(
     turbowasm_reader *section,
     turbowasm_module_summary *summary,
-    const turbowasm_validation_context *context);
+    turbowasm_validation_context *context);
 
 #endif /* TURBOWASM_VALIDATE_DATA_H */
